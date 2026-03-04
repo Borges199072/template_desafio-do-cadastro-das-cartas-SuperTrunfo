1cffb59530f4c7d7c45471ed2cd96c076406ec5d
Borges199072 <gilberto_pa10@hotmail.com>
2026-03-04 23:52:01 +0000

    Realizado o desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,48 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  
+  char estado[2];
+  char codigo[4];
+  char nome[50];
+  int populacao;
+  float area;
+  float PIB; 
+  int numero;
 
   // Área para entrada de dados
 
+  printf ("Digite o estado: \n");
+  scanf ("%s", &estado);
+
+  printf ("Digite o codigo da carta: \n");
+  scanf ("%s", &codigo);
+
+  printf ("Digite o nome do estado: \n");
+  scanf ("%s", &nome);
+
+  printf ("Digite a população: \n");
+  scanf ("%d", &populacao);
+
+  printf ("Digite a area (em km²): \n");
+  scanf ("%f", &area);
+
+  printf ("Digite o PIB: \n");
+  scanf ("%f", &PIB);
+
+  printf ("Digite o numero de pontos turisticos: \n");
+  scanf ("%d", &numero);
+
+  
   // Área para exibição dos dados da cidade
+
+  printf ("estado: %s\n", estado);
+  printf ("Codigo da carta: %s\n", codigo);
+  printf ("Nome do estado: %s\n", nome);
+  printf ("População: %d\n", populacao);
+  printf ("Area (em km²): %f\n", area);
+  printf ("PIB: %f\n", PIB);
+  printf ("Numero de pontos turisticos: %d\n", numero);
 
 return 0;
 } 
